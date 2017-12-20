26a79cfca45b05df668f6ef9cefc563e2c7a0314
Andres Gonzalez <andres@3months.com>
2017-12-20 17:36:39 +1300

    WIP for factory

--- contracts/SimpleMultisig.sol
+++ contracts/SimpleMultisig.sol
@@ -1,45 +1,46 @@
 pragma solidity 0.4.18;
+
+
 contract SimpleMultisig {
 
-  uint public nonce;                // (only) mutable state
-  uint public threshold;            // immutable state
-  mapping (address => bool) public isOwner; // immutable state
-  address[] public ownersArr;        // immutable state
-  uint public nOwners;
+    uint public nonce;                          // (only) mutable state
+    uint public threshold;                      // immutable state
+    mapping (address => bool) public isOwner;   // immutable state
+    address[] public ownersArr;                 // immutable state
+    uint public nOwners;
 
-  function SimpleMultisig(uint threshold_, address[] owners_) public {
-    require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ != 0);
-
-    address lastAdd = address(0);
-    for (uint i=0; i<owners_.length; i++) {
-      require(owners_[i] > lastAdd);
-      isOwner[owners_[i]] = true;
-      lastAdd = owners_[i];
-    }
-    ownersArr = owners_;
-    nOwners = owners_.length;
-    threshold = threshold_;
-  }
-
-  // Note that address recovered from signatures must be strictly increasing
-  function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes data) public {
-    require(sigR.length == threshold);
-    require(sigR.length == sigS.length && sigR.length == sigV.length);
-
-    // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
-    bytes32 txHash = keccak256(byte(0x19), byte(0), address(this), destination, value, data, nonce);
-
-    address lastAdd = address(0); // cannot have address(0) as an owner
-    for (uint i = 0; i < threshold; i++) {
-        address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
-        require(recovered > lastAdd && isOwner[recovered]);
-        lastAdd = recovered;
+    function SimpleMultisig(uint _threshold, address[] _owners) public {
+        require(_owners.length <= 10 && _threshold <= _owners.length && _threshold != 0);
+        address lastAdd = address(0);
+        for (uint i=0; i < _owners.length; i++) {
+            require(_owners[i] > lastAdd);
+            isOwner[_owners[i]] = true;
+            lastAdd = _owners[i];
+        }
+        ownersArr = _owners;
+        nOwners = _owners.length;
+        threshold = _threshold;
     }
 
-    // If we make it here all signatures are accounted for
-    nonce = nonce + 1;
-    require(destination.call.value(value)(data));
-  }
+    function () public payable {}
 
-  function () public payable {}
+    // Note that address recovered from signatures must be strictly increasing (refer to line 16)
+    function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes data) public {
+        require(sigR.length == threshold);
+        require(sigR.length == sigS.length && sigR.length == sigV.length);
+
+        // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
+        bytes32 txHash = keccak256(byte(0x19), byte(0), address(this), destination, value, data, nonce);
+
+        address lastAdd = address(0); // cannot have address(0) as an owner
+        for (uint i = 0; i < threshold; i++) {
+            address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > lastAdd && isOwner[recovered]);
+            lastAdd = recovered;
+        }
+
+        // If we make it here all signatures are accounted for
+        nonce = nonce + 1;
+        require(destination.call.value(value)(data));
+    }
 }
